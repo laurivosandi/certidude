ffdab4d36d32a5cb170ab824252f594f01a605c0
Lauri Võsandi <lauri.vosandi@gmail.com>
2016-03-01 13:52:10 +0200

    Update strongSwan leftupdown script

--- doc/strongswan-updown.sh
+++ doc/strongswan-updown.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-cat << EOF | curl -X POST -d @- -H "Event-Type: $PLUTO_VERB" http://ca.example.com/pub/?id=CA-channel-identifier-goes-here
-{"address": "$PLUTO_PEER_SOURCEIP","peer": "$PLUTO_PEER","identity": "$PLUTO_PEER_ID","routed_subnet": "$PLUTO_MY_CLIENT"}
+cat << EOF | curl -s -X POST -d @- -H "X-EventSource-Event: $PLUTO_VERB" http://ca.example.com/pub/?id=CA-channel-identifier-goes-here
+{"address": "$(echo $PLUTO_PEER_CLIENT  | sed 's/\/32$//')", "peer": "$PLUTO_PEER", "identity": "$PLUTO_PEER_ID", "routed_subnet": "$PLUTO_MY_CLIENT"}
 EOF
