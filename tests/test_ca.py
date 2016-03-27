f88a970e2a4af0c8152659b005fff75e0499b18b
Lauri Võsandi <lauri.vosandi@gmail.com>
2016-03-28 00:18:41 +0300

    Attempt to fix CA test

--- tests/test_ca.py
+++ tests/test_ca.py
@@ -1,25 +1,14 @@
 from click.testing import CliRunner
 from certidude.cli import entry_point as cli
 
-
-from certidude.wrappers import CertificateAuthorityConfig
-
 runner = CliRunner()
 
 def test_ca_config():
     # Authority setup
     with runner.isolated_filesystem():
-        result = runner.invoke(cli, ['setup', 'authority', 'xca'])
+        result = runner.invoke(cli, ['setup', 'authority'])
         assert not result.exception
 
-        # Load CA
-        conf = CertificateAuthorityConfig('./xca/openssl.cnf.example')
-
-        assert conf.ca_list == ['xca']
-
-        ca = conf.instantiate_authority('xca')
-
-        cert = ca.certificate
-
-        assert cert.serial_number == '0000000000000000000000000000000000000001'
+        from certidude import authority
+        assert authority.certificate.serial_number == '0000000000000000000000000000000000000001'
         # TODO: Figure out a way to properly test cert.signed, cert.expires, cert.digest, etc
