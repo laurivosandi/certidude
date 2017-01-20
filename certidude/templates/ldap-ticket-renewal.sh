175f7f5d53fc0a930d0a37b86988c272c40555fc
Lauri Võsandi <lauri.vosandi@gmail.com>
2017-01-20 10:56:46 +0000

    Fixes for LDAP access using machine credentials

--- certidude/templates/ldap-ticket-renewal.sh
+++ certidude/templates/ldap-ticket-renewal.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-KRB5CCNAME={{ticket_path}}.part kinit -k {{name}}$ -S ldap/dc1.{{domain}}@{{realm}} -t /etc/krb5.keytab
-chown certidude:certidude {{ticket_path}}.part
-mv {{ticket_path}}.part {{ticket_path}}
+mkdir -p /run/certidude
+KRB5CCNAME=/run/certidude/krb5cc.part kinit -k {{name}}$ -S ldap/dc1.{{domain}}@{{realm}} -t /etc/krb5.keytab
+chown certidude:certidude /run/certidude/krb5cc.part
+mv /run/certidude/krb5cc.part /run/certidude/krb5cc
 
